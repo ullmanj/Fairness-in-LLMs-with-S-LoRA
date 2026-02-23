2385c9747eefe75707427ea71a7f0563a0c23b3d
ullmanj <jamieMullman@gmail.com>
2026-02-23 11:26:55 -0800

    Create a separate queue class that handles the logic around which clients have requests activlely in the queue etc

--- slora/server/fairness/fair_queue.py
+++ slora/server/fairness/fair_queue.py
@@ -33,12 +33,8 @@ class FairQueue(ReqQueue):
         # WE ASSUME: 1:1 mapping between adapter_dir and client.
         self.counters_by_client: dict[str, int] = { adapter_dir: 0 for adapter_dir in self.adapter_dirs }  # initialize to 0 for all clients
 
-        # Mechanism by which we track which clients are in the queue — functions like semaphores.
-        self.num_queued_requests_by_client: dict[str, int] = { }
-
-        # Request queue is accessed from self.waiting_req_list in the parent class.
-
-        self.client_that_made_Q_empty_when_last_request_left = None
+        # Our own shadow queue whose functions will update the parent queue (self.waiting_req_list) as well.
+        self.queued_requests = QueuedRequests()
 
         # NOTE: This is unused. If we want to incoroprate tiered fariness, we can incorporate this in the implemenetation below.
         self.fair_weights = fair_weights
@@ -51,28 +47,27 @@ class FairQueue(ReqQueue):
     # manager.py for the prior steps in this loop.
     def append(self, req: Req):
         # if another request does NOT exist in the queue from the same client
-        if self.num_queued_requests_by_client.get(req.adapter_dir, 0) == 0:
+        if self.queued_requests.get_earliest_request_from_client(req.adapter_dir) is None:
             # if the queue is empty
             if len(self.waiting_req_list) == 0:
                 # get the counter of the last client who left the Q (call it c_l)
                 c_l = 0
-                if self.client_that_made_Q_empty_when_last_request_left is not None:
-                    c_l = self.counters_by_client[self.client_that_made_Q_empty_when_last_request_left]
+                if self.queued_requests.client_that_made_Q_empty_when_last_request_left is not None:
+                    c_l = self.counters_by_client[self.queued_requests.client_that_made_Q_empty_when_last_request_left]
                 
                 # set the counter for this request's client (call it c_u) to the max{ c_u, c_l }
                 self.counters_by_client[req.adapter_dir] = max(self.counters_by_client[req.adapter_dir], c_l)
             # else
             else:
                 # Build a set "P" of all the clients who have a request in the queue
-                P = [client for client in self.num_queued_requests_by_client.keys() if self.num_queued_requests_by_client[client] > 0]
+                P = self.queued_requests.get_clients_in_queue()
                 # Get the minimum counter for those clients in P and call it c_min
                 c_min = min(self.counters_by_client[client] for client in P)
                 # Update the counter for this request's client (call it c_u) to the max{ c_u, c_min }
                 self.counters_by_client[req.adapter_dir] = max(self.counters_by_client[req.adapter_dir], c_min)
 
-        # enqueue as normal into the waiting_req_list (queue source of truth)
-        super().append(req)
-        self.num_queued_requests_by_client[req.adapter_dir] = self.num_queued_requests_by_client.get(req.adapter_dir, 0) + 1
+        # enqueue as normal
+        self.queued_requests.push_request(req, self.waiting_req_list)
         return
 
     # ############# With Execution stream (i.e. whose turn is it next? Build a batch.) #############
@@ -92,15 +87,14 @@ class FairQueue(ReqQueue):
         # while true
         while True:
             # get the client (call it k) whose counter is the smallest among all of the clients in the queue.
-            clients_in_queue = [client for client in self.num_queued_requests_by_client.keys() if self.num_queued_requests_by_client.get(client, 0) > 0]
+            clients_in_queue = self.queued_requests.get_clients_in_queue()
             k = min(clients_in_queue, key=lambda client: self.counters_by_client[client])
             # let r be the earlist request in the queue from client k.
-            r = self._get_earliest_request_from_client(k)
+            r = self.queued_requests.get_earliest_request_from_client(k)
 
             # Not in algorithm: Drop the aborted requests. Inspired by super()
             if r.aborted:
-                self.waiting_req_list.remove(r)
-                self.num_queued_requests_by_client[r.adapter_dir] = self.num_queued_requests_by_client.get(r.adapter_dir, 0) - 1
+                self.queued_requests.remove_request(r, self.waiting_req_list)
                 continue
             
             # if r cannot fit into the batch, break. (because we have filled the batch maximally while staying fair)
@@ -113,12 +107,7 @@ class FairQueue(ReqQueue):
             new_batch_reqs.append(r)
             new_batch_total_tokens += r.input_len
             # Remove r from the queue
-            self.waiting_req_list.remove(r)
-            self.num_queued_requests_by_client[r.adapter_dir] = self.num_queued_requests_by_client.get(r.adapter_dir, 0) - 1
-            if self.waiting_req_list == []:
-                self.client_that_made_Q_empty_when_last_request_left = k
-            else:
-                self.client_that_made_Q_empty_when_last_request_left = None
+            self.queued_requests.remove_request(r, self.waiting_req_list)
         
         if len(new_batch_reqs) == 0:
             return None
@@ -138,21 +127,6 @@ class FairQueue(ReqQueue):
             self.counters_by_client[client] += self.w_q_output * L
         return
     
-
-    # ############ HELPER FUNCTIONS ############
-    # Format the queue into a dictionary of requests by client.
-    def _get_requests_by_client_from(self, requests: List[Req]) -> dict[str, List[Req]]:
-        requests_by_client = { adapter_dir: [] for adapter_dir in self.adapter_dirs }
-        for req in requests:
-            requests_by_client[req.adapter_dir].append(req)
-        return requests_by_client
-    # Get the earliest request from a client.
-    def _get_earliest_request_from_client(self, client: str) -> Req:
-        for req in self.waiting_req_list:
-            if req.adapter_dir == client:
-                return req
-        return None
-    
     # ############ OVERRIDE HELPER FUNCTIONS ############
     
     #  _init_cache_list(self, current_batch:Batch, lora_ranks and
@@ -167,3 +141,33 @@ class FairQueue(ReqQueue):
 
 
 
+class QueuedRequests:
+    def __init__(self):
+        # need to maintain an official queue because the ReqQueue official queue is accessed by other parts of the code.
+        self.queued_requests_by_client: dict[str, Req] = { }
+        self.client_that_made_Q_empty_when_last_request_left = None
+    
+    def get_earliest_request_from_client(self, client: str) -> Req:
+        return self.queued_requests_by_client.get(client, None)
+
+    def push_request(self, request: Req, official_queue: List[Req]):
+        official_queue.append(request)
+        self.queued_requests_by_client = self.queued_requests_by_client.get(request.adapter_dir, []) + [request]
+
+    def remove_request(self, request: Req, official_queue: List[Req]):
+        official_queue.remove(request)
+        if self.queued_requests_by_client.get(request.adapter_dir, None) is None:
+            raise ValueError(f"Shadow queue for client {request.adapter_dir} does not exist.")
+        self.queued_requests_by_client[request.adapter_dir].remove(request)
+
+        # remove that client from our shadow queue to reflect in the keys that there are no requests from this client in the queue.
+        if len(self.queued_requests_by_client[request.adapter_dir]) == 0:
+            self.queued_requests_by_client.pop(request.adapter_dir)
+        
+        if self.official_queue == []:
+            self.client_that_made_Q_empty_when_last_request_left = request.adapter_dir
+        else:
+            self.client_that_made_Q_empty_when_last_request_left = None
+    
+    def get_clients_in_queue(self) -> List[str]:
+        return list(self.queued_requests_by_client.keys())
